67677107b641c270aaa397d349b0a3efa8a2e74d
Poshini Ganesh <49205289+Poshini@users.noreply.github.com>
2021-05-29 22:09:15 +0530

    Update server.spec.js

--- specification/server.spec.js
+++ specification/server.spec.js
@@ -7,7 +7,7 @@ describe('get messages', () => {
             expect(res.statusCode).toEqual(200)
             done()
         })
-    })
+    }) 
     it('should return a list, thats not empty', (done) => {
         request.get('http://localhost:3000/messages', (err, res) => {
             expect(JSON.parse(res.body).length).toBeGreaterThan(0)
